564236cbf0fbcfcb121317b16933af1ce2395f3c
Brian Cohan <briancohan@gmail.com>
2023-12-09 13:06:12 -0500

    2023-12-09

---
+++ 2023/Day-09/aoc_2023_09.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+from typing import NewType
+
+Data = NewType("Data", list[tuple[int]])
+
+
+def parse_input(full: bool = True, file_name: Path = None) -> Data:
+    if file_name is None:
+        file_name = "input.txt" if full else "sample.txt"
+    input_file = Path(__file__).parent / file_name
+
+    return [
+        tuple(map(int, line.split()))
+        for line in input_file.read_text().strip().splitlines()
+    ]
+
+
+def get_next(numbers: tuple[int], forecast: bool = True) -> int:
+    next_line = tuple(b - a for a, b in zip(numbers, numbers[1:]))
+    if all(i == 0 for i in next_line):
+        return numbers[-1] if forecast else numbers[0]
+    else:
+        if forecast:
+            return get_next(next_line, forecast) + numbers[-1]
+        else:
+            return numbers[0] - get_next(next_line, forecast)
+
+
+def part_1(data: Data) -> int:
+    return sum(get_next(numbers) for numbers in data)
+
+
+def part_2(data: Data) -> int:
+    return sum(get_next(numbers, False) for numbers in data)
+
+
+if __name__ == "__main__":
+    data = parse_input()
+
+    print(f"Part 1: {part_1(data)}")
+    print(f"Part 2: {part_2(data)}")
