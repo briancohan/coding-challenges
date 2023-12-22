8ce8fadfea00a33adb93a1a387f3e0def97a1a73
Brian Cohan <briancohan@gmail.com>
2023-12-21 21:38:41 -0500

    2015-12-01

--- 2015/Day-01/aoc_2015_01.py
+++ 2015/Day-01/aoc_2015_01.py
@@ -1,36 +1,26 @@
-import json
-import logging
 from pathlib import Path
 from typing import NewType
 
-Data = NewType("Data", list[str])
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(message)s",
-    filename=Path(__file__).with_suffix(".log"),
-    filemode="w",
-)
+Data = NewType("Data", str)
 
 
 def parse_input(full: bool = True, file_name: Path = None) -> Data:
     if file_name is None:
         file_name = "input.txt" if full else "sample.txt"
     input_file = Path(__file__).parent / file_name
-    return input_file.read_text().strip().splitlines()
+    return input_file.read_text().strip()
 
 
 def part_1(data: Data) -> int:
-    return 0
+    return data.count("(") - data.count(")")
 
 
 def part_2(data: Data) -> int:
-    return 0
+    return next(i for i in range(len(data) + 1) if part_1(data[:i]) == -1)
 
 
 if __name__ == "__main__":
-    data = parse_input(False)
-    logging.debug(json.dumps(data, indent=4))
+    data = parse_input()
 
     print(f"Part 1: {part_1(data)}")
     print(f"Part 2: {part_2(data)}")
