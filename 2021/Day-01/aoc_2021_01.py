6b478148bf7f0ef89af6a4d6d82d3cfd7130819c
Brian Cohan <briancohan@gmail.com>
2023-12-22 09:59:06 -0500

    2021-12-01

--- 2021/Day-01/aoc_2021_01.py
+++ 2021/Day-01/aoc_2021_01.py
@@ -1,36 +1,36 @@
-import json
-import logging
+from functools import reduce
 from pathlib import Path
 from typing import NewType
 
 Data = NewType("Data", list[str])
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(message)s",
-    filename=Path(__file__).with_suffix(".log"),
-    filemode="w",
-)
 
 
 def parse_input(full: bool = True, file_name: Path = None) -> Data:
     if file_name is None:
         file_name = "input.txt" if full else "sample.txt"
     input_file = Path(__file__).parent / file_name
-    return input_file.read_text().strip().splitlines()
+    return list(map(int, input_file.read_text().strip().splitlines()))
 
 
 def part_1(data: Data) -> int:
-    return 0
+    return reduce(
+        lambda r, i: (r[0] + (1 if i > r[1] else 0), i),
+        data,
+        (0, float("inf")),
+    )[0]
 
 
 def part_2(data: Data) -> int:
-    return 0
+    return sum(
+        [
+            sum(data[i + 0 : i + 3]) < sum(data[i + 1 : i + 4])
+            for i in range(len(data) - 3)
+        ]
+    )
 
 
 if __name__ == "__main__":
-    data = parse_input(False)
-    logging.debug(json.dumps(data, indent=4))
+    data = parse_input()
 
     print(f"Part 1: {part_1(data)}")
     print(f"Part 2: {part_2(data)}")
