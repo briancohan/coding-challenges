816840e904a4fd4cd7fc6dec915260d4f2c7ef2d
Brian Cohan <briancohan@gmail.com>
2023-12-22 09:40:43 -0500

    2019-12-01

--- 2019/Day-01/aoc_2019_01.py
+++ 2019/Day-01/aoc_2019_01.py
@@ -1,36 +1,34 @@
-import json
-import logging
 from pathlib import Path
 from typing import NewType
 
-Data = NewType("Data", list[str])
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(message)s",
-    filename=Path(__file__).with_suffix(".log"),
-    filemode="w",
-)
+Data = NewType("Data", list[int])
 
 
 def parse_input(full: bool = True, file_name: Path = None) -> Data:
     if file_name is None:
         file_name = "input.txt" if full else "sample.txt"
     input_file = Path(__file__).parent / file_name
-    return input_file.read_text().strip().splitlines()
+    return list(map(int, input_file.read_text().strip().splitlines()))
+
+
+def fuel_load(mass: int) -> int:
+    return (mass // 3) - 2
 
 
 def part_1(data: Data) -> int:
-    return 0
+    return sum([fuel_load(i) for i in data])
 
 
 def part_2(data: Data) -> int:
-    return 0
+    fuel = 0
+    for i in data:
+        while (i := (i // 3) - 2) > 0:
+            fuel += i
+    return fuel
 
 
 if __name__ == "__main__":
-    data = parse_input(False)
-    logging.debug(json.dumps(data, indent=4))
+    data = parse_input()
 
     print(f"Part 1: {part_1(data)}")
     print(f"Part 2: {part_2(data)}")
