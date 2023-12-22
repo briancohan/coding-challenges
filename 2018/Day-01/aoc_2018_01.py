fad90999e07171a2db1797e4af0184c639cccd93
Brian Cohan <briancohan@gmail.com>
2023-12-21 21:39:10 -0500

    2018-12-01

--- 2018/Day-01/aoc_2018_01.py
+++ 2018/Day-01/aoc_2018_01.py
@@ -1,36 +1,35 @@
-import json
-import logging
 from pathlib import Path
 from typing import NewType
+from collections import deque
 
-Data = NewType("Data", list[str])
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(message)s",
-    filename=Path(__file__).with_suffix(".log"),
-    filemode="w",
-)
+Data = NewType("Data", list[int])
 
 
 def parse_input(full: bool = True, file_name: Path = None) -> Data:
     if file_name is None:
         file_name = "input.txt" if full else "sample.txt"
     input_file = Path(__file__).parent / file_name
-    return input_file.read_text().strip().splitlines()
+    return list(map(int, input_file.read_text().strip().splitlines()))
 
 
 def part_1(data: Data) -> int:
-    return 0
+    return sum(data)
 
 
 def part_2(data: Data) -> int:
-    return 0
+    _data = deque(data)
+    frequencies = []
+    frequency = 0
+    while True:
+        frequency += _data[0]
+        if frequency in frequencies:
+            return frequency
+        frequencies.append(frequency)
+        _data.rotate(-1)
 
 
 if __name__ == "__main__":
-    data = parse_input(False)
-    logging.debug(json.dumps(data, indent=4))
+    data = parse_input()
 
     print(f"Part 1: {part_1(data)}")
     print(f"Part 2: {part_2(data)}")
--- 2018/Day-01/test_2018_01.py
+++ 2018/Day-01/test_2018_01.py
@@ -6,19 +6,19 @@ full_input = challenge.parse_input()
 
 def test_part_1_sample_input():
     result = challenge.part_1(sample_input)
-    assert result == 0
+    assert result == 3
 
 
 def test_part_1_full_input():
     result = challenge.part_1(full_input)
-    assert result == 0
+    assert result == 416
 
 
 def test_part_2_sample_input():
     result = challenge.part_2(sample_input)
-    assert result == 0
+    assert result == 2
 
 
 def test_part_2_full_input():
     result = challenge.part_2(full_input)
-    assert result == 0
+    assert result == 56752
