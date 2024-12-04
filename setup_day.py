9f036f8aa1c0425c28c6e3dd4d703f5a01aee08f
Brian Cohan <briancohan@gmail.com>
2024-12-04 13:08:31 -0500

    tweak for formatting

--- setup_day.py
+++ setup_day.py
@@ -5,15 +5,21 @@ from datetime import datetime
 base_dir = Path(__file__).parent
 template_dir = base_dir / "tmpl"
 
+
 def fix_date(s: str, year: int, day: int) -> str:
     return s.replace("yyyy", str(year)).replace("dd", f"{day:02}")
 
-default_year = datetime.now().year
-next_day = max([int(d.name.split('-')[1]) for d in base_dir.joinpath(str(default_year)).iterdir()]) + 1
 
-parser = argparse.ArgumentParser(description='Setup a new day for Advent of Code')
-parser.add_argument('--year', type=int, help='The year to setup for', default=default_year)
-parser.add_argument('--day', type=int, help='The day to setup', default=next_day)
+default_year = datetime.now().year
+last_day = max(
+    [int(d.name.split("-")[1]) for d in base_dir.joinpath(str(default_year)).iterdir()]
+)
+
+parser = argparse.ArgumentParser(description="Setup a new day for Advent of Code")
+parser.add_argument(
+    "--year", type=int, help="The year to setup for", default=default_year
+)
+parser.add_argument("--day", type=int, help="The day to setup", default=last_day + 1)
 args = parser.parse_args()
 
 challenge_dir = base_dir.joinpath(str(args.year), f"Day-{args.day:02}")
